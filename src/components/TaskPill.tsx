a9c825144d182c77a582aec0f3655cd83f544162
Oscar <oscar.sbd@hotmail.com>
2023-10-03 00:58:56 +0200

    Updated styles. from bootstrap to chakra ui Updated database.json

--- src/components/TaskPill.tsx
+++ src/components/TaskPill.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import {encodeSomething} from '../helpers/encoding'
 import {compateTwoDates} from '../helpers/dates'
-import {Button, HStack, Text} from '@chakra-ui/react'
-
+import {
+    Button,
+    ButtonGroup,
+    HStack,
+    Popover,
+    PopoverArrow,
+    PopoverBody,
+    PopoverCloseButton,
+    PopoverContent,
+    PopoverHeader,
+    PopoverTrigger,
+    Text
+} from '@chakra-ui/react'
 
 export const TaskPill = (props: any) => {
     const {item, diaActual, today, subject} = props
@@ -18,6 +29,7 @@ export const TaskPill = (props: any) => {
         overflow: 'none',
         borderLeft: '',
         borderRight: '',
+        borderRadius: 8,
         boxShadow: '0 0 3px gray',
         border: '1px solid white'
     }
@@ -27,21 +39,63 @@ export const TaskPill = (props: any) => {
         stylePill.border = 'none'
     }
 
+    const codeId = encodeSomething(item)
 
+    const toggleAtLocalStorage = (codeId: string) => () => {
+        let started = localStorage.getItem(codeId)
+        if (started) {
+            localStorage.removeItem(codeId)
+        } else {
+            localStorage.setItem(codeId, new Date().toISOString())
+        }
+        window.location.reload()
+        console.log('saved')
+    }
+    const isToggledAtLocalStorage = localStorage.getItem(codeId)
     // if (!assignedClass) return null
-    return (
-        <Button
-            id={hasStartedToday ? encodeSomething(item) : ''}
-            size={'xs'}
-            colorScheme={subject.colorScheme}
-            {...stylePill}
-            {...styleContainer}
-            title={item.task}>
-            <HStack justify={'space-evenly'} align={'center'}>
-                {hasStartedToday && <Text m={0}>😎</Text>}
-                <Text m={0}>{item.task}</Text>
-                {lastDay && <Text m={0}>😨</Text>}
-            </HStack>
-        </Button>
+    return (<ButtonGroup gap={0}
+            {...stylePill}>
+            {isToggledAtLocalStorage && <Popover>
+                <PopoverTrigger>
+                    <Button p={0} m={0} size={'xs'}>❔</Button>
+                </PopoverTrigger>
+                <PopoverContent>
+                    <PopoverArrow />
+                    <PopoverCloseButton />
+                    <PopoverHeader></PopoverHeader>
+                    <PopoverBody><Text
+                        m={0}>✅ {(new Date(isToggledAtLocalStorage)).toLocaleDateString()}</Text></PopoverBody>
+                </PopoverContent>
+            </Popover>
+            }
+
+
+        {hasStartedToday && <Button
+                m={0}
+                size={'xs'}>
+                <HStack justify={'space-evenly'} align={'center'}>
+                   <Text m={0}>😎</Text>
+                </HStack>
+            </Button>}
+            <Button
+                id={hasStartedToday ? codeId : ''}
+                m={0}
+                size={'xs'}
+                colorScheme={subject.colorScheme}
+                {...styleContainer}
+                onClick={toggleAtLocalStorage(codeId)}
+                title={item.task}>
+                <HStack justify={'space-evenly'} align={'center'}>
+                    <Text m={0}>{item.task}</Text>
+                </HStack>
+            </Button>
+        {lastDay &&  <Button
+                m={0}
+                size={'xs'}>
+                <HStack justify={'space-evenly'} align={'center'}>
+                   <Text m={0}>😨</Text>
+                </HStack>
+            </Button>}
+        </ButtonGroup>
     )
 }
