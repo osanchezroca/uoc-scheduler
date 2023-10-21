4f3ebc40bf1583764ea8cfb3d9ae53b7dfd3f2c1
Oscar <oscar.sbd@hotmail.com>
2023-10-21 03:03:46 +0200

    Added title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,14 @@ import type {AppProps} from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import dynamic from 'next/dynamic'
 import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head'
 
 const App = ({Component, pageProps}: AppProps) => {
     return (
         <ChakraProvider>
+            <Head>
+                <title>UOC Scheduler</title>
+            </Head>
             <Component {...pageProps} />
         </ChakraProvider>
     )
